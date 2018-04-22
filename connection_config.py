1a84c973fbe15e813b597d0b36da367401b3a4b0
Mirko Savasta <savastamirko@gmail.com>
2018-04-22 15:30:07 +0200

    Add files via upload

---
+++ connection_config.py
@@ -0,0 +1,12 @@
+
+## DB Credentials
+## db: default db
+
+config = {
+
+"MySQLfwd":{"ip": "192.168.12.18", "user":"mysqlfwd","psw":None, "remote_port":22, "local_port": None},
+"Retail":{"ip":"192.168.90.32", "user":"mirkosavasta", "psw":"xZiwot87sa", "remote_port":3306, "local_port": 3306, "db": None },
+"CRM":{"ip":"192.168.12.2", "user":"mirkosavasta", "psw":"EfS4JMGcSW" ,"remote_port":3306, "local_port": 3307, "db": "crm"},
+"DWH":{"ip":"192.168.12.71", "user":"mirkosavasta", "psw":"m2hZ7twvnU" ,"remote_port":3306, "local_port": 3308, "db":"dwg"}
+
+}
